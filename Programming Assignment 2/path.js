5c411e666520d3bedb7736b187a5f95453100285
Steven Le <136869060+sle2001@users.noreply.github.com>
2023-09-30 15:32:06 -0500

    Update path.js

--- Programming Assignment 2/path.js
+++ Programming Assignment 2/path.js
@@ -1,1 +1,12 @@
-
+function CirclePath(x,y,r) {
+    "use strict";
+    this.x = x || 200;
+    this.y = y || 200;
+    this.r = r || 100;
+}
+CirclePath.prototype.eval = function(u) {
+    "use strict";
+    var x = this.r * Math.cos(u);
+    var y = this.r * Math.sin(u);
+    return [x + this.x,y + this.y ,-y,x];
+}
